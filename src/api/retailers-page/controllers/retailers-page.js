adb56eade2b5d0e5466bf1da5c37106f564ca951
fvillano <francesco.villano@msccruises.com>
2026-03-28 19:51:14 +0100

    feat: add new content types for collectors, corporate, newsroom, personalized, retailers, and watches pages with SEO attributes

---
+++ src/api/retailers-page/controllers/retailers-page.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::retailers-page.retailers-page');
---
+++ src/api/watches-page/controllers/watches-page.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::watches-page.watches-page');
